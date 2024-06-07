4d3d13a19497699117db36e6254412c98d98b6a6
gothmate <gothmate@gmail.com>
2024-06-07 14:38:01 -0300

    fix(Home): - finalizado mock da home + aside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,21 +17,31 @@ export default function Home() {
       {!session.isLogged ?
         <>
           <Header />
-          <div className={styles.main}>
-            <main className={styles.content}>
-                <nav>
-                  <ul className={styles.list}>
-                    <li>
-                      <Link href={'/'}>Pratos</Link>
-                    </li>
-                    <li>
-                      <Link href={'/'}>Bebidas</Link>
-                    </li>
-                    <li>
-                      <Link href={'/'}>Sobremesas</Link>
-                    </li>
-                  </ul>
-                </nav>
+          <main className={styles.main}>
+            <div className={styles.content}>
+                <h5>Chamado #0001452</h5>
+                <div className={styles.navCart}>
+                  <nav>
+                    <ul className={styles.list}>
+                      <li>
+                        <Link href={'/'}>Pratos</Link>
+                      </li>
+                      <li>
+                        <Link href={'/'}>Bebidas</Link>
+                      </li>
+                      <li>
+                        <Link href={'/'}>Sobremesas</Link>
+                      </li>
+                    </ul>
+                  </nav>
+                  <Image
+                    className={styles.cart} 
+                    width={30}
+                    height={30}
+                    src={'/cart.svg'}
+                    alt="Carrinho"
+                  />
+                </div>
                 <div className={styles.cardsGroup}>
                   <div className={styles.cards}>
                     <Image 
@@ -89,12 +99,76 @@ export default function Home() {
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae veniam illum reprehenderit nesciunt facere beatae blanditiis quos distinctio sint?</p>
                     </div>
                   </div>
+                  <div className={styles.cards}>
+                    <Image 
+                      className={styles.img}
+                      width={300}
+                      height={130}
+                      src={'/img.png'}
+                      alt='comida'
+                      priority
+                    />
+                    <div className={styles.infoGroup}>
+                      <h3>Nome do Prato</h3>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae veniam illum reprehenderit nesciunt facere beatae blanditiis quos distinctio sint?</p>
+                    </div>
+                  </div>
+                  <div className={styles.cards}>
+                    <Image 
+                      className={styles.img}
+                      width={300}
+                      height={130}
+                      src={'/img.png'}
+                      alt='comida'
+                      priority
+                    />
+                    <div className={styles.infoGroup}>
+                      <h3>Nome do Prato</h3>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae veniam illum reprehenderit nesciunt facere beatae blanditiis quos distinctio sint?</p>
+                    </div>
+                  </div>
+                  <div className={styles.curtains}>.</div>
                 </div>
-            </main> 
-            <aside className={styles.aside}>
-              <h1>Teste</h1>
+            </div> 
+            <aside className={styles.aside} id={styles.asideHome}>
+              <div className={styles.math}>
+                <div>
+                  <h1>Total</h1>
+                  <div>
+                    <h4>Frango Xadrez</h4>
+                    <ul>
+                      <li>Arroz</li>
+                      <li>Feijão</li>
+                      <li>Farofa</li>
+                      <li>Fritas</li>
+                      <li>Salada de Alface</li>
+                    </ul>
+                    <p>Valor R$16,00</p>
+                  </div>
+                  <div>
+                    <h4>Filé de Alcatra</h4>
+                    <ul>
+                      <li>Arroz</li>
+                      <li>Feijão</li>
+                      <li>Farofa</li>
+                      <li>Fritas</li>
+                      <li>Salada de Legumes</li>
+                    </ul>
+                    <p>Valor R$18,00</p>
+                  </div>
+                </div>
+                <div className={styles.valorFinal}>
+                  <Image
+                    width={30}
+                    height={30}
+                    src={'/cart.svg'}
+                    alt="Carrinho"
+                  /> 
+                  Valor: R$34,00
+                </div>
+              </div>
             </aside>
-          </div>
+          </main>
           <Footer />
         </>
         :
